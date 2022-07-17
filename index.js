b7a51790e8041cfc0624ba6c9faaf8f68d84b2bf
Arman Yeghiazaryan <otanim617@gmail.com>
2022-07-17 19:27:37 +0400

    delay test
    Signed-off-by: Arman Yeghiazaryan <otanim617@gmail.com>

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 const LED = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
 
-const blinkInterval = setInterval(blinkLED, 250); //run the blinkLED function every 250ms
+const blinkInterval = setInterval(blinkLED, 50); //run the blinkLED function every 250ms
 
 function blinkLED() { //function to start blinking
   console.log('blink led');
