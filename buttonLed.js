b78d0a637fc3a7703148b14af735adf74a4e1a78
Arman Yeghiazaryan <otanim617@gmail.com>
2022-07-18 23:15:55 +0400

    button click test

--- buttonLed.js
+++ buttonLed.js
@@ -3,13 +3,14 @@ const LED = new Gpio(4, 'out'); //use GPIO pin 4 as output
 const pushButton = new Gpio(16, 'in', 'both'); //use GPIO pin 17 as input, and 'both' button presses, and releases should be handled
 
 let oldValue = 0;
+let counter = 0;
 pushButton.watch( (err, value) => { //Watch for hardware interrupts on pushButton GPIO, specify callback function
   if (err) { //if an error
     return console.error('There was an error', err); //output error message to
   }
 
   if (oldValue !== value) {
-    console.log(`BUTTON STATE: ${value}`);
+    console.log(`${++counter} BUTTON STATE: ${value}`);
   }
 
   oldValue = value;
