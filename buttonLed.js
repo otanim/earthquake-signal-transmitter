3b2cea08ea524beca5159f17a763e9c472bc1e49
Arman Yeghiazaryan <otanim617@gmail.com>
2022-07-18 23:14:56 +0400

    button click test

--- buttonLed.js
+++ buttonLed.js
@@ -2,21 +2,26 @@ const Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 const LED = new Gpio(4, 'out'); //use GPIO pin 4 as output
 const pushButton = new Gpio(16, 'in', 'both'); //use GPIO pin 17 as input, and 'both' button presses, and releases should be handled
 
+let oldValue = 0;
 pushButton.watch( (err, value) => { //Watch for hardware interrupts on pushButton GPIO, specify callback function
   if (err) { //if an error
     return console.error('There was an error', err); //output error message to
   }
 
-  console.log(`BUTTON STATE: ${value}`);
-  LED.writeSync(value); //turn LED on or off depending on the button state (0 or 1)
+  if (oldValue !== value) {
+    console.log(`BUTTON STATE: ${value}`);
+  }
+
+  oldValue = value;
+  //LED.writeSync(value); //turn LED on or off depending on the button state (0 or 1)
 });
 
 function unexportOnClose() { //function to run when exiting program
-  LED.writeSync(0); // Turn LED off
-  LED.unexport(); // Unexport LED GPIO to free resources
+  //LED.writeSync(0); // Turn LED off
+  //LED.unexport(); // Unexport LED GPIO to free resources
   pushButton.unexport(); // Unexport Button GPIO to free resources
 }
 
-LED.writeSync(1); //debug
+//LED.writeSync(1); //debug
 
 process.on('SIGINT', unexportOnClose); //function to run when user closes using ctrl+c
